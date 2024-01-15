6bb5e8f6d35c50eae905ca4ddcd09e269ad63862
Nicholas Hoang <nchoang1@uci.edu>
2024-01-15 14:52:08 -0800

    Starting functions for basic shape

---
+++ a0.py
@@ -0,0 +1,18 @@
+# a0.py
+
+# Starter code for assignment 0 in ICS 32 Programming with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+
+# Nicholas Hoang
+# nchoang1@uci.edu
+# 24588398
+
+def horizontal():
+    print('+-+')
+
+def horizontal_two():
+    print('+-+-+')
+
+def vertical():
+    print('| |')
